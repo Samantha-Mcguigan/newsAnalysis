4826f82d808c2d6546e52539f77354c91cf135a5
Aaron Newton <ajn54@pitt.edu>
2017-04-24 21:05:25 -0400

    changed the website

--- Website/search.php
+++ Website/search.php
@@ -14,7 +14,8 @@ $input_search = htmlspecialchars($_GET['input_search']);
     <link rel="stylesheet" type="text/css" href="style.css"/>
   </head>
   <body>
-    <!--#include virtual="menu.html" -->
+    <script type="text/javascript" src="buttons.js" ></script>
+        <!--#include virtual="menu.html" -->
     <h2><img id="searchIcon" src="search-icon.png" alt="search icon" width="30"/> Search results</h2>
     <p>Our search retrieves <strong>exact matches</strong>, as well as <strong>close matches</strong> retrieved either by <dfn>ngram searches</dfn> (for portions of the string you entered) or by <dfn>Levenshtein distance</dfn> (or terms calculated as a close match with a few characters of difference). Clicking on the highlighted match term in the search results leads to the full article as posted by its host news organization.</p>
     <p><a href="search.html">Return to Search page.</a></p>
@@ -22,5 +23,6 @@ $input_search = htmlspecialchars($_GET['input_search']);
 $xql = REST_PATH . "/db/queries/newsAnalysisKwic.xql?input_search=$input_search";
 echo file_get_contents($xql);
 ?>
+<!--#include virtual="bottom.html" -->
 </body>
 </html>
