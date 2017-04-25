d5f4c3f5007fd10c32819ad5a1f10f5358dc58e6
Aaron Newton <ajn54@pitt.edu>
2017-04-24 21:25:56 -0400

    more website additions

--- Website/search.php
+++ Website/search.php
@@ -12,13 +12,18 @@ $input_search = htmlspecialchars($_GET['input_search']);
 
     <title>News Analysis: Search</title>
     <link rel="stylesheet" type="text/css" href="style.css"/>
+    <script type="text/javascript" src="buttons.js" ></script>
+    <script type="text/javascript" src="search.js">//</script>
   </head>
   <body>
-    <script type="text/javascript" src="buttons.js" ></script>
+    
         <!--#include virtual="menu.html" -->
     <h2><img id="searchIcon" src="search-icon.png" alt="search icon" width="30"/> Search results</h2>
+    <div class="text">
+            <p>Enter a word or search string in the search box below to find its occurrence and context in our collection of news articles. Our search retrieves <strong>exact matches</strong>, as well as <strong>close matches</strong> retrieved either by <dfn>ngram searches</dfn> (for portions of the string you entered) or by <dfn>Levenshtein distance</dfn> (or terms calculated as a close match with a few characters of difference).</p>
+            <p><img id="searchIcon" src="search-icon.png" alt="search icon" width="18"/> <input id="search" type="text" size="25" placeholder="press [enter] to search"/></p>
+        </div>
     <p>Our search retrieves <strong>exact matches</strong>, as well as <strong>close matches</strong> retrieved either by <dfn>ngram searches</dfn> (for portions of the string you entered) or by <dfn>Levenshtein distance</dfn> (or terms calculated as a close match with a few characters of difference). Clicking on the highlighted match term in the search results leads to the full article as posted by its host news organization.</p>
-    <p><a href="search.html">Return to Search page.</a></p>
 <?php
 $xql = REST_PATH . "/db/queries/newsAnalysisKwic.xql?input_search=$input_search";
 echo file_get_contents($xql);
